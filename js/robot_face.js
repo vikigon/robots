d1cd30e63831d4e2163039984793584a7259bc1f
Viki Gon <viki.gon@gmail.com>
2020-07-19 19:33:25 -0300

    TP Canvas correcciones.

--- js/robot_face.js
+++ js/robot_face.js
@@ -5,13 +5,11 @@ var eyeHeight = 60;
 var canvasCenterX = 100;
 
 // carita
-ctx.beginPath();
 ctx.fillStyle = '#02a0ef';
 ctx.fillRect(13, 30, 175, 90); 
 ctx.fill();
     
 // ojos
-ctx.beginPath();
 ctx.arc(60, eyeHeight, eyeRadius, 0, 2 * Math.PI);
 ctx.arc(140, eyeHeight, eyeRadius, 0, 2 * Math.PI);
 ctx.fillStyle = '#e6d417';
@@ -19,15 +17,17 @@ ctx.fill();
   
 // boca
 ctx.beginPath();
-ctx.arc(canvasCenterX, 78, 30, 0, Math.PI);
+ctx.arc(canvasCenterX, 78, 30, 0, Math.PI, false);
+ctx.closePath();
 ctx.fillStyle = '#de4b4b';
 ctx.fill();
 
 //antenita
 ctx.beginPath();
 ctx.fillStyle = '#02a0ef';
-ctx.fillRect(canvasCenterX, 20, 7, 10); 
 ctx.fill();
+ctx.fillRect(canvasCenterX, 20, 7, 10); 
+ctx.closePath();
 ctx.beginPath();
 ctx.arc(canvasCenterX + 3.5, 12, 7, 0, 2 * Math.PI);
 ctx.lineWidth = 2;
@@ -35,13 +35,16 @@ ctx.strokeStyle = '#ffffff';
 ctx.stroke();
 ctx.fillStyle = '#6632cf';
 ctx.fill();
+ctx.closePath();
 
 //orejas
-ctx.beginPath()
+ctx.beginPath();
 ctx.fillStyle = '#02a0ef';
 ctx.fillRect(1, 50, 12, 30); 
 ctx.fill();
-ctx.beginPath()
+ctx.closePath();
+ctx.beginPath();
 ctx.fillStyle = '#02a0ef';
 ctx.fillRect(187, 50, 12, 30); 
 ctx.fill();
+ctx.closePath();
